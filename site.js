54e8d377ecd0d014bb01ef687b6ad45e66da2e54
Kaiden Brunke <700233885@fvtc.edu>
2024-09-12 20:43:44 -0500

    added welcome message

--- site.js
+++ site.js
@@ -1,1 +1,16 @@
-console.log("It is linked correctly");
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+const h2 = document.querySelector('h2')
+if(isMorning){
+    h2.textContent = "Good Morning"
+}else if(isAfternoon){
+    h2.textContent = "Good Afternoon"
+}else if( isEvening)
+{
+    h2.textContent = "Good Evening"
+}else{
+    h2.textContent = "Hello"
+}
